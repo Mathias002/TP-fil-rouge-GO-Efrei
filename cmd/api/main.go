7b617e5aa92003719bb3dd135b8a36d26cbb546a
Mathias002 <moussetmathias@gmail.com>
2025-11-04 23:30:30 +0100

    Refactor main entry to use app and store packages
    Replaces manual flag parsing and menu logic with a call to app.Run using a new memory store from the storage package. This simplifies main.go and centralizes application logic in the internal/app and internal/store packages.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,60 +1,13 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-
-	"github.com/Mathias002/TP-fil-rouge-GO-efrei/contact"
-	"github.com/Mathias002/TP-fil-rouge-GO-efrei/main_menu"
+	"github.com/Mathias002/TP-fil-rouge-GO-efrei/internal/app"
+	"github.com/Mathias002/TP-fil-rouge-GO-efrei/internal/store"
 )
 
 func main() {
-	// On init les contacts par défaut
-	contact.Init()
+	// init storer
+	var store = storage.NewMemoryStore()
 
-	// On Init les flags
-	addName := flag.String("name", "", "Nom du contact")
-	addEmail := flag.String("email", "", "Email du contact")
-
-	// On Parse les flags
-	flag.Parse()
-
-	// On vérifie que des flags sont fournis <> si oui on ajoute le contact sinon on lance le menu interactif
-	if *addName != "" && *addEmail != "" {
-		contact.AddContactFlag(contact.NameContact(*addName), contact.EmailContact(*addEmail))
-		return // Quitter après l'ajout
-	}
-
-	// boucle infini pour l'affichage du menu en continu
-	Loop_Main_Menu:
-		for { // boucle infinie
-
-			// Récupération du choix saisi par l'utilisateur.rice
-			choice := main_menu.Main_menu()
-			// if err != nil {
-			// 	fmt.Println("Erreur : " + err)
-			// 	break
-			// }
-
-			// switch sur les options du menu
-			// en fonction du choix on appele les fonctions correspondantes
-			switch choice {
-			// Ajout d'un contact 
-			case 1:
-				contact.AddContact()
-			// Affichage des contacts 
-			case 2:
-				contact.Contacts.DisplayContacts()
-			// Mise à jour d'un contact
-			case 3:
-				contact.UpdateContact()
-			// Suppression d'un contact
-			case 4:
-				contact.DeleteContact()
-			// Fermeture du programme 
-			case 5:
-				fmt.Println("Merci de votre visite à bientôt ! 🫡")
-				break Loop_Main_Menu
-			}
-		}
+    app.Run(store)
 }
