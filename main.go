baf7ead11bc6f314f53150f9f220785b403d1c58
Mathias002 <moussetmathias@gmail.com>
2025-11-10 18:19:45 +0100

    Refactor main to use cmd.Execute
    Replaces direct app and store initialization in main.go with a call to cmd.Execute, streamlining the entry point and delegating execution to the cmd package.

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/Mathias002/TP-fil-rouge-GO-efrei/internal/app"
-	"github.com/Mathias002/TP-fil-rouge-GO-efrei/internal/store"
+	"github.com/Mathias002/TP-fil-rouge-GO-efrei/cmd"
 )
 
 func main() {
-	// init storer
-	var store = storage.NewMemoryStore()
-
-    app.Run(store)
+	cmd.Execute()
 }
